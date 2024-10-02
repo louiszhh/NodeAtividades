7fe8324e7a84bbe448478f91f95222e4c1231a78
Luis Henryque <luishenryquenovakoski@gmail.com>
2024-10-02 20:37:43 -0300

    Final commit

--- Express.js
+++ Express.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const dados = require('./dados.json'); 
+const fs = require('fs');
+const path = require('path');
+const dados = require('./dados.json');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <body>
-        <p>Esta é uma página html.</p>
-      </body>
-    </html>
-  `);
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, 'utf-8', (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Erro ao carregar a página.');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
 });
 
 app.get('/api/data', (req, res) => {
-  res.json(dados); 
+  res.json(dados);
 });
 
 app.listen(PORT, () => {
